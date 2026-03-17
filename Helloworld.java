4a3df3bcb80badb35f2825d496021c4e7b9dca6a
Om-12342 <os9192@srmist.edu.in>
2026-03-17 13:58:26 +0530

    UC2 Completed

--- Helloworld.java
+++ Helloworld.java
@@ -1,10 +1,8 @@
 public class Helloworld {
     public static void main(String[] args) {
 
-        // UC1
         System.out.println("Hello World");
 
-        // UC2
         if (args.length > 0) {
             String name = args[0];
             System.out.println("Hello, " + name + "! Welcome to the application.");
